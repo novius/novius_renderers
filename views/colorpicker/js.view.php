9e76f3b96e65e646f543e37ea49835293fcf8c44
Julian Esperat <esperat@novius.fr>
2013-11-20 11:14:20 +0100

    Compatibilité colorpicker avec common_field

--- views/colorpicker/js.view.php
+++ views/colorpicker/js.view.php
@@ -3,31 +3,41 @@
         'jquery-nos',
         'static/apps/lib_renderers/js/colorpicker',
         'link!static/apps/lib_renderers/css/colorpicker.css'
-    ], function( $nos ) {
-    $nos(function() {
+    ], function( $ ) {
+    $(function() {
 
-            $nos('#<?= $id ?>').after('<div id="renderer_<?= $id ?>" class="customRenderer"/>');
-			$nos('#renderer_<?= $id ?>').append('<div class="colorSelector"/>');
-			$nos('#renderer_<?= $id ?> div.colorSelector').after('<div class="colorpickerHolder"/>');
-			$nos('#renderer_<?= $id ?> div.colorSelector').append('<div/>');
-            var color = '#' + $nos('#<?= $id ?>').attr('value');
-			$nos('#renderer_<?= $id ?> div.colorSelector div').css('backgroundColor', color);
+        var $input = $('#<?= $id ?>');
+        $input.wrap('<div id="renderer_<?= $id ?>" class="customRenderer"/>');
+        // On wrap un div display none pour forcer le non-affichage
+        // (cas du common field qui rajoute un display:block sur le champ ça pète l'affichage)
+        $input.wrap('<div style="display:none;"></div>');
+        var $renderer = $('#renderer_<?= $id ?>');
+        $renderer.append('<div class="colorSelector"/>');
+        $('#renderer_<?= $id ?> div.colorSelector').after('<div class="colorpickerHolder"/>');
+        $('#renderer_<?= $id ?> div.colorSelector').append('<div/>');
+        var color = '#' + $input.val();
+        $('#renderer_<?= $id ?> div.colorSelector div').css('backgroundColor', color);
 
-			$nos('#renderer_<?= $id ?> div.colorpickerHolder').ColorPicker({
-                flat: true,
-                color: color,
-                onSubmit: function(hsb, hex, rgb) {
-                    $nos('#renderer_<?= $id ?> div.colorSelector div').css('backgroundColor', '#' + hex);
-                    $nos('#<?= $id ?>').attr('value', hex);
-                    $nos('#renderer_<?= $id ?> div.colorpickerHolder').stop().animate({height: 0}, 500);
-                }
-            });
-            $nos('#renderer_<?= $id ?> div.colorpickerHolder>div').css('position', 'absolute');
-            var widt = false;
-            $nos('#renderer_<?= $id ?> div.colorSelector').on('click', function() {
-                $nos('#renderer_<?= $id ?> div.colorpickerHolder').stop().animate({height: widt ? 0 : 173}, 500);
-                widt = !widt;
-            });
+        $('#renderer_<?= $id ?> div.colorpickerHolder').ColorPicker({
+            flat: true,
+            color: color,
+            onSubmit: function(hsb, hex, rgb) {
+                $('#renderer_<?= $id ?> div.colorSelector div').css('backgroundColor', '#' + hex);
+                $input.attr('value', hex);
+                $('#renderer_<?= $id ?> div.colorpickerHolder').stop().animate({height: 0}, 500);
+            }
         });
+        $('#renderer_<?= $id ?> div.colorpickerHolder>div').css('position', 'absolute');
+        var width = false;
+        $('#renderer_<?= $id ?> div.colorSelector').on('click', function() {
+            $('#renderer_<?= $id ?> div.colorpickerHolder').stop().animate({height: width ? 0 : 173}, 500);
+            width = !width;
+        });
+
+        // Si c'est un common field, alors l'overlay qui affiche la popup aura été calculé avant (pas assez haut)
+        if ($input.is('[context_common_field]')) {
+            $renderer.next().height('36px');
+        }
     });
+});
 </script>
