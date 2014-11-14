9a2f2eca3c567040d0ba72a1c3463233d6772b6d
Victor ALBERT <albert@novius.fr>
2014-11-14 11:18:24 +0100

    take "order" option into account before rendering sort buttons

--- views/hasmany/item.view.php
+++ views/hasmany/item.view.php
@@ -17,20 +17,20 @@ echo $fieldset->build_hidden_fields();
             )) ?>><?= __('Duplicate this item') ?></button>
         </div>
         <div class="right">
+<?php
+if (!empty($options['order'])) {
+?>
             <button class="hasmany_button_move_down item-down-js" data-icon="triangle-1-s">&nbsp;</button>
             <button class="hasmany_button_move_up item-up-js" data-icon="triangle-1-n">&nbsp;</button>
+<?php
+}
+?>
             <button class="hasmany_button_delete ui-state-error item-delete-js" data-icon="trash" <?= array_to_attr(array(
                 'data-question' => __('Are you sure you want to delete this item?'),
                 'data-removed' => empty($is_new) ? __('This item will be deleted when the form is saved') : '',
             )) ?>>&nbsp;</button>
         </div>
     </div>
-<?php
-if (!empty($options['order'])) {
-?>
-<?php
-}
-?>
     <div class="hasmany_content">
         <table>
 <?php
