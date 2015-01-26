aded65d9c759e9898773c5719a27643c99ce7b84
foine <brousse@novius.fr>
2015-01-26 09:53:04 +0100

    Reformat Code

--- classes/renderer/multimedias.php
+++ classes/renderer/multimedias.php
@@ -8,13 +8,13 @@ class Renderer_MultiMedias extends Renderer
 {
     protected static $DEFAULT_RENDERER_OPTIONS = array(
         'limit' => false,
-        'mode' => 'all',
+        'mode'  => 'all',
     );
 
     public function build()
     {
         $attr_id = $this->get_attribute('id');
-        $id = !empty($attr_id) ? $attr_id : uniqid('multimedias_');
+        $id      = !empty($attr_id) ? $attr_id : uniqid('multimedias_');
         $this->fieldset()->append(static::js_init($id));
         $key = !empty($this->renderer_options['key']) ? $this->renderer_options['key'] : $this->name;
 
@@ -23,21 +23,21 @@ class Renderer_MultiMedias extends Renderer
             $this->set_value($this->getValueFromInstance($item, $key));
         }
 
-        return (string) \View::forge('novius_renderers::multimedias/inputs', array(
-            'id' => $id,
-            'key' => $key,
+        return (string)\View::forge('novius_renderers::multimedias/inputs', array(
+            'id'      => $id,
+            'key'     => $key,
             'options' => $this->renderer_options,
-            'item' => $item,
-            'value' => $this->value,
+            'item'    => $item,
+            'value'   => $this->value,
         ), false);
     }
 
     protected function getValueFromInstance($item, $key)
     {
         $value = array();
-        $index=1;
-        while(!empty($item->medias->{$key.$index})) {
-            $media = $item->medias->{$key.$index};
+        $index = 1;
+        while (!empty($item->medias->{$key . $index})) {
+            $media = $item->medias->{$key . $index};
             if (!empty($media)) {
                 $value[$index] = $media->id;
                 $index++;
