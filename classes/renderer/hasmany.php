1bdc364ff4374c2f0fe26bfd36308c4adfc7d454
Martin Smeeckaert <smeeckaert@novius.fr>
2016-01-20 08:51:34 +0100

    check if the item is populated by classes

--- classes/renderer/hasmany.php
+++ classes/renderer/hasmany.php
@@ -64,7 +64,8 @@ class Renderer_HasMany extends \Nos\Renderer
         $values = \Arr::get($data, $name);
         $postData = \Input::post($name);
         $item->$name = array();
-        if(!empty($values) && empty($postData)) {
+        $isPopulatedWithItem = !empty($values) && is_object(current($values));
+        if(empty($values) || ($isPopulatedWithItem && empty($postData))) {
             // When the input array is empty (which happens when the user tries to remove all childs),
             // the relation array (array(id => Model)) is given instead, which prevents us to remove the childs from database.
             return true;
